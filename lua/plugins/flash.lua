89f31c9279317990341201873b9c8f4032370cd8
ACupofAir <thoughts.times@gmail.com>
2024-05-24 20:38:17 +0800

    [update] config the flash key to <f>, and surround key to <s>

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,6 +1,18 @@
 return {
   {
     "folke/flash.nvim",
+    keys = {
+      -- disable the default flash keymap
+      { "s", mode = { "n", "x", "o" }, false },
+      {
+        "f",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+    },
     opts = {
       modes = {
         char = {
