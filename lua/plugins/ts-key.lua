212136ed9c5c907aa22412f770d9d7408677fb1f
ACupofAir <thoughts.times@gmail.com>
2024-12-24 00:08:57 +0800

    add ts-key change

---
+++ lua/plugins/ts-key.lua
@@ -0,0 +1,54 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  keys = {
+    { "<c-k>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  opts_extend = { "ensure_installed" },
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-k>",
+        node_incremental = "<C-k>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+  },
+  ---@param opts TSConfig
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
